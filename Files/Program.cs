d084b2e7ef9df1a5333998f8cd12dc6d37feb01c
Marcos Marins <desenvolvimento1@macbarbosa.com.br>
2019-11-22 10:13:44 -0300

    Iniciando git e primeira aula File e FileInfo

---
+++ Files/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\mac1005\source\repos\Files\file1.txt";
+            string targetPath = @"C:\Users\mac1005\source\repos\Files\file2.txt";
+
+            try
+            {
+
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
