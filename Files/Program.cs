acfcf021c8f103ac5528fec4dea872e984b6ba6e
Marcos Marins <marcos-henrique2000@hotmail.com>
2019-11-25 11:37:40 -0300

    Subindo exercicio feito

--- Files/Program.cs
+++ Files/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Globalization;
 
 namespace Files
 {
@@ -7,16 +8,44 @@ namespace Files
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\mac1005\source\repos\Files\file1.txt";
+            string sourcePath = @"C:\Users\mac1005\source\repos\Files\vendidos.csv";
+            
+            StreamReader sr = null;
 
-            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
-            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
-            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
-            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
-            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
-            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
-            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
-            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+            try
+            {
+                sr = File.OpenText(sourcePath);
+                var sep = Path.DirectorySeparatorChar;
+                var path = Path.GetDirectoryName(sourcePath);
+                
+                Directory.CreateDirectory(path + sep + "out");
+
+                string targetPath = path + sep + "out" + sep + "sumary.csv";
+
+                using(StreamWriter sw = File.AppendText(targetPath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string info = sr.ReadLine();
+
+                        string[] doc = info.Split(",");
+
+                        string product = doc[0];
+                        double price = double.Parse(doc[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(doc[2]);
+
+                        double total = quantity * price;
+
+                        sw.WriteLine($"{product},{total.ToString("F2", CultureInfo.InvariantCulture)}");
+                    }
+                }
+
+            }catch(IOException e)
+            {
+                Console.WriteLine(e);
+            }
+
         }
     }
 }
+//https://github.com/acenelio/files1-csharp/blob/master/Course/Program.cs
